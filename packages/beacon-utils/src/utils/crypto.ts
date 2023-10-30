9e49d7a61c71d6169731a683706db3baff1f9975
Tristan Allaire <tristan.allaire@alarch.fr>
2023-10-30 11:00:47 +0100

    bs58check package updated

--- packages/beacon-utils/src/utils/crypto.ts
+++ packages/beacon-utils/src/utils/crypto.ts
@@ -184,7 +184,7 @@ export async function getAddressFromPublicKey(publicKey: string): Promise<string
       if (publicKey.startsWith(key) && publicKey.length === value.length) {
         prefix = value.prefix
         const decoded = bs58check.decode(publicKey)
-        plainPublicKey = decoded.slice(key.length, decoded.length).toString('hex')
+        plainPublicKey = Buffer.from(decoded.slice(key.length, decoded.length)).toString('hex')
         break
       }
     }
